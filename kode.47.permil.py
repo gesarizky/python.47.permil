6f57d93862fe163aba51d40e9c5b2265e45d402e
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-11-04 16:04:38 +0700

    Add files via upload

---
+++ kode.47.permil.py
@@ -0,0 +1,20 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+
+# Menginput
+print('Masukan Nilai Pecahan a / b ')
+a = int(input("Masukan Nilai a : "))
+b = int(input("Masukan Nilai b : "))
+
+# Mengkonversi
+desimal = a / b
+permil = desimal * 1000
+
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Hasil Permil = ',permil)
+print('Maka Hasil Desimal = ',desimal)
+print('==========================================================')
